87a9b6c63849acf4c58fc7059291771bb262c758
Dharun Naraynan <lkdharun@gmail.com>
2022-09-22 14:38:15 +0530

    sec

--- js/main1.js
+++ js/main1.js
@@ -18,3 +18,36 @@ inputs.forEach(input => {
 	input.addEventListener("focus", addcl);
 	input.addEventListener("blur", remcl);
 });
+
+function Elogin(){
+	var email = document.getElementById("email").value;
+	var password = document.getElementById("password").value;
+	console.log(email);
+	console.log(password);
+	window.location.href = "home.html";
+	if(email == "lkdharun@gmail.com" && password == "123456"){
+		window.location.href = "home.html";
+	}
+}
+
+function checkmail(){
+    strRegExp = /[a-z0-9._%+-]+@[a-z0-9.-]+\.[a-z]{2,3}$/;
+    val=document.getElementById("email").value.trim();
+    input=document.getElementById("email");
+
+    if(document.getElementById("email").value.trim().length == 0){
+       input.classList.add("fail");
+       input.classList.remove("success");
+        document.getElementById("remail").innerHTML="Field cannot be Empty";
+    }
+        else if(!val.match(strRegExp)){
+           input.classList.remove("success");
+        input.classList.add("fail");
+        document.getElementById("remail").innerHTML = "Incorrect Format";
+        }
+    else{
+        input.classList.remove("fail");
+        input.classList.add("success");
+        document.getElementById("remail").innerHTML="";
+    }
+}
